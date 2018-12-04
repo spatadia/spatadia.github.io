93647a93f77ed16d0a65a82ca362ca2281f678f3
Sagar Patadia <35351813+spatadia@users.noreply.github.com>
2018-12-04 00:32:30 -0500

    Add files via upload

---
+++ app/time-clock/time-clock.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-time-clock',
+  templateUrl: './time-clock.component.html',
+  styleUrls: ['./time-clock.component.css']
+})
+export class TimeClockComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+
+  }
+
+}
+
